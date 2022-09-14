c64a9aecbaf672b7ebf18d6118e19e6d5e5a00ec
Andrii Dibrov <dibrov2a@gmail.com>
2022-09-14 12:13:28 +0300

    Externalized validation properties and messages to 'validation.properties'

--- src/main/java/com/clearsolutions/testassignment/web/validation/MinimumAgeValidator.java
+++ src/main/java/com/clearsolutions/testassignment/web/validation/MinimumAgeValidator.java
@@ -1,15 +1,19 @@
 package com.clearsolutions.testassignment.web.validation;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 import java.time.Period;
 
+@Component
+@PropertySource("classpath:validation.properties")
 public class MinimumAgeValidator implements ConstraintValidator<MinimumAge, LocalDate> {
 
-    @Value("${validation.user.age.minimum.years}")
+    @Value("${user.age.minimum.years}")
     private Integer minimumUserAgeYears;
 
     @Override
